47166a0533cba49682c66b1b756f5cde5ae2b680
Jonathan Lee <jonathan.h.lee@gmail.com>
2017-10-26 15:03:37 -0700

    Assignment 4 completed

--- models/address_book.rb
+++ models/address_book.rb
@@ -1,18 +1,31 @@
 # #8 - load library entry.rb relative to address_book.rb's file path
 require_relative 'entry'
 
-  class AddressBook
-    attr_reader :entries
+class AddressBook
+  attr_reader :entries
 
-    def initialize
-      @entries = []
+  def initialize
+    @entries = []
+  end
+
+  def remove_entry(name, phone_number, email)
+    delete_entry = nil
+
+    @entries.each do |entry|
+      if name == entry.name && phone = entry.phone_number && email == entry.email
+        delete_entry = entry
+      end
     end
+
+    @entries.delete(delete_entry)
+    
+  end
 
   def add_entry(name, phone_number, email)
     # #9 - create variable to store insertion index
     index = 0
     entries.each do |entry|
-    # #10 - compare name to current entry to find index to insert at
+      # #10 - compare name to current entry to find index to insert at
       if name < entry.name
         break
       end
--- spec/address_book_spec.rb
+++ spec/address_book_spec.rb
@@ -18,6 +18,23 @@ RSpec.describe AddressBook do
     end
   end
 
+  describe "remove_entry" do
+    it "removes an entry using the name, phone, and email address" do
+      book = AddressBook.new
+      book.add_entry("Michael Faraday", "010.210.5181", "michael.faraday@faraday.com")
+
+      name = "Ada Lovelace"
+      phone_number = "010.012.1815"
+      email_address = "august.king@lovelace.com"
+      book.add_entry(name, phone_number, email_address)
+
+      expect(book.entries.size).to eq 2
+      book.remove_entry(name, phone_number, email_address)
+      expect(book.entries.size).to eq 1
+      expect(book.entries.first.name).to eq("Michael Faraday")
+    end
+  end
+
   describe "#add_entry" do
     it "adds only one entry to the address book" do
       book = AddressBook.new
