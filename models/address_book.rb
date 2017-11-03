bde123fb7702714de45e8e72098f2c966e7faad6
Jonathan Lee <jonathan.h.lee@gmail.com>
2017-11-03 01:29:24 -0700

    Checkpoint 6 reading CSVs: completed

--- models/address_book.rb
+++ models/address_book.rb
@@ -1,5 +1,6 @@
 # #8 - load library entry.rb relative to address_book.rb's file path
 require_relative 'entry'
+require "csv"
 
   class AddressBook
     attr_reader :entries
@@ -20,5 +21,17 @@ require_relative 'entry'
     end
     # #11 - insert new entries using calculated index
     entries.insert(index, Entry.new(name, phone_number, email))
+  end
+
+  # #7 - The result of CSV.parse is an object of type CSV::Table
+  def import_from_csv(file_name)
+    # Implementation goes here
+    csv_text = File.read(file_name)
+    csv = CSV.parse(csv_text, headers: true, skip_blanks: true)
+    # #8 - iterate over the CSV::Table object's rows, create a hash for each row, and convert each row_hash to an Entry 
+    csv.each do |row|
+      row_hash = row.to_hash
+      add_entry(row_hash["name"], row_hash["phone_number"], row_hash["email"])
+    end
   end
 end
